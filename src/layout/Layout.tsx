0f718c585196b2ac3442de1eefbca2bda519291b
guiss26 <guissella584@gmail.com>
2026-08-03 21:29:27 +0200

    feat(app): remove routing for single-page architecture Remove router/Router.tsx Remove RouterProvider from main.tsx Simplify Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import App from "../App";
 
 export default function Layout() {
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Navbar />
-            <Outlet />
+            <div className="flex-1">
+                {/* <Outlet /> */}
+                <App />
+            </div>
             <Footer />
-        </>
+        </div>
     )
 }
