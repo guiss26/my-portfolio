e4cf010e9983fb9239329bdff1a2373dd1bf60f1
guiss26 <guissella584@gmail.com>
2026-08-05 22:16:10 +0200

    feat(hero): implement dark mode

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,13 +4,13 @@ import { BsArrowUpRight } from "react-icons/bs";
 export default function Hero() {
     return (
         <>
-            <section id="hero" className="scroll-mt-20 md:flex">
+            <section id="hero" className="scroll-mt-20 md:flex dark:bg-neutral-950 pt-10">
                 {/* Izquierda */}
                 <div className="border-b border-neutral-300 md:border-r md:border-black md:w-2/5 p-10">
                     <p className="flex items-center gap-3 text-xs tracking-widest text-neutral-400"><span className="w-3 h-3 bg-lime-400 border border-neutral-950"></span> [ STATUS: READY ]</p>
 
-                    <div className="border border-neutral-950 p-5 mt-6 bg-white">
-                        <div className="flex items-center justify-between border-b border-neutral-300 pb-1">
+                    <div className="border border-neutral-950 p-5 mt-6 bg-white dark:bg-neutral-900 dark:border-white">
+                        <div className="flex items-center justify-between border-b border-neutral-300 dark:border-neutral-600 pb-1">
                             <p className="uppercase text-neutral-400 text-[11px] tracking-widest">~ / portfolio - bash</p>
 
                             <div className="flex gap-2">
@@ -21,27 +21,27 @@ export default function Hero() {
                         </div>
 
                         <ul className="mt-3">
-                            <li className="text-xs/5 text-neutral-600 font-mono"><span className="text-neutral-300">$</span> whoami</li>
-                            <li className="text-xs/5 text-neutral-600 font-mono"><span className="text-neutral-300"> &gt;</span> guissella_perez</li>
-                            <li className="text-xs/5 text-neutral-600 font-mono"><span className="text-neutral-300">$</span> cat status.txt</li>
-                            <li className="text-xs/5 text-neutral-600 font-mono"><span className="text-neutral-300">&gt;</span> disponible para trabajar</li>
+                            <li className="text-xs/5 text-neutral-600 font-mono dark:text-neutral-400"><span className="text-neutral-300 dark:text-neutral-500">$</span> whoami</li>
+                            <li className="text-xs/5 text-neutral-600 font-mono dark:text-neutral-400"><span className="text-neutral-300 dark:text-neutral-500"> &gt;</span> guissella_perez</li>
+                            <li className="text-xs/5 text-neutral-600 font-mono dark:text-neutral-400"><span className="text-neutral-300 dark:text-neutral-500">$</span> cat status.txt</li>
+                            <li className="text-xs/5 text-neutral-600 font-mono dark:text-neutral-400"><span className="text-neutral-300 dark:text-neutral-500">&gt;</span> disponible para trabajar</li>
                         </ul>
                     </div>
 
                     <dl className="space-y-4 text-xs font-mono mt-6">
                         <div className="grid grid-cols-2 md:flex md:flex-col md:gap-2 lg:flex-row lg:justify-between">
                             <dt className="text-neutral-500 lg:w-1/2">ROL</dt>
-                            <dd className="text-[12px] lg:w-1/2">Desarrolladora Frontend Junior</dd>
+                            <dd className="text-[12px] lg:w-1/2 dark:text-neutral-300">Desarrolladora Frontend Junior</dd>
                         </div>
 
                         <div className="grid grid-cols-2 md:flex md:flex-col md:gap-2 lg:flex-row lg:justify-between">
                             <dt className="text-neutral-500 lg:w-1/2">UBICACIÓN</dt>
-                            <dd className="text-[12px] lg:w-1/2">Madrid, España</dd>
+                            <dd className="text-[12px] lg:w-1/2 dark:text-neutral-300">Madrid, España</dd>
                         </div>
 
                         <div className="grid grid-cols-2 md:flex md:flex-col md:gap-2 lg:flex-row lg:justify-between">
                             <dt className="text-neutral-500 lg:w-1/2">EMAIL</dt>
-                            <dd className="text-[12px] lg:w-1/2">guissellapl@gmail.com</dd>
+                            <dd className="text-[12px] lg:w-1/2 dark:text-neutral-300">guissellapl@gmail.com</dd>
                         </div>
                     </dl>
                 </div>
@@ -54,18 +54,18 @@ export default function Hero() {
                     </div>
 
                     <h1 className="leading-none font-serif-libertinus text-8xl mt-5">
-                        <span className="block text-neutral-900">Guissella</span>
+                        <span className="block text-neutral-900 dark:text-neutral-100">Guissella</span>
                         <span className="flex items-center gap-3">
-                            <span className="block italic text-neutral-500">Pérez</span>
+                            <span className="block italic text-neutral-500 dark:text-neutral-400">Pérez</span>
                             <span className="w-8 h-15 bg-lime-400 animate-terminal-blink border-l-2 border-neutral-920"></span>
                         </span>
                     </h1>
 
-                    <p className="text-neutral-600 tracking-normal font-mono mt-4 text-sm">Apasionada por crear experiencias digitales a través de interfaces con criterio, código limpio y curiosidad digital.</p>
+                    <p className="text-neutral-600 tracking-normal font-mono mt-4 text-sm dark:text-neutral-300">Apasionada por crear experiencias digitales a través de interfaces con criterio, código limpio y curiosidad digital.</p>
 
                     <div className="flex gap-3 text-[10px] md:text-[13px] mt-8 tracking-widest">
-                        <a href="#projects" className="flex items-center gap-3 uppercase text-lime-400 border bg-neutral-950 py-1 px-4 hover:text-neutral-950 hover:bg-lime-400 hover:border border-neutral-950">Ver proyectos <BsArrowDownRight /> </a>
-                        <a href="#contact" className="flex items-center gap-3 uppercase border py-1 px-3 hover:text-lime-400 hover:bg-neutral-950 hover:border-neutral-950">Trabajemos juntos <BsArrowUpRight /> </a>
+                        <a href="#projects" className="flex items-center gap-3 uppercase text-lime-400 border dark:bg-neutral-200 dark:hover:bg-lime-400 dark:text-black dark:hover:text-black bg-neutral-950 py-1 px-4 hover:text-neutral-950 hover:bg-lime-400 hover:border border-neutral-950">Ver proyectos <BsArrowDownRight /> </a>
+                        <a href="#contact" className="flex items-center gap-3 uppercase border py-1 px-3 dark:border-neutral-200 dark:text-neutral-200 dark:hover:text-black dark:hover:bg-neutral-200 hover:text-lime-400 hover:bg-neutral-950 hover:border-neutral-950">Trabajemos juntos <BsArrowUpRight /> </a>
                     </div>
                 </div>
             </section>
