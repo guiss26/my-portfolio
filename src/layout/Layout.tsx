1115a8b00c5edeaf5c2120bdb898cb0ec20813a2
guiss26 <guissella584@gmail.com>
2026-08-05 11:54:17 +0200

    chore(layout): remove unused comments

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,3 @@
-// import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import App from "../App";
@@ -8,7 +7,6 @@ export default function Layout() {
         <div className="flex flex-col min-h-screen">
             <Navbar />
             <div className="flex-1">
-                {/* <Outlet /> */}
                 <App />
             </div>
             <Footer />
