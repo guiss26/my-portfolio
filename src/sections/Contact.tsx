b6033837b394c9121634f1ecad62cab87023f1fe
guiss26 <guissella584@gmail.com>
2026-08-06 17:33:27 +0200

    feat(contact): implement dark mode

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -58,19 +58,19 @@ export default function Contact() {
 
     return (
         <>
-            <section id="contact" className=" scroll-mt-20 font-mono md:flex">
-                <div className="md:w-2/5 py-15 px-10 border-b border-neutral-300 md:border-r md:border-black">
+            <section id="contact" className=" scroll-mt-20 font-mono md:flex dark:bg-neutral-950">
+                <div className="md:w-2/5 py-15 px-10 border-b dark:border-neutral-200 border-neutral-300 md:border-r md:border-black">
                     <p className="uppercase font-mono text-xs text-neutral-400">§ 04 - Última página</p>
-                    <h1 className="text-5xl font-serif-libertinus mt-4"> Hablemos</h1>
-                    <p className="font-mono text-neutral-500 text-xs my-6">Si estás buscando alguien junior con criterio, ganas y tiempo para aprender, hablemos. Respondo en menos de 24h.</p>
+                    <h1 className="text-5xl font-serif-libertinus mt-4 dark:text-white"> Hablemos</h1>
+                    <p className="font-mono text-neutral-500 text-xs my-6 dark:text-neutral-300">Si estás buscando alguien junior con criterio, ganas y tiempo para aprender, hablemos. Respondo en menos de 24h.</p>
 
-                    <a className="flex items-center gap-2 text-xs/7 text-neutral-800" href="mailto:guissellapl@gmail.com"><MdOutlineMailOutline className="text-neutral-500"/> guissellapl@gmail.com</a>
-                    <a className="flex items-center gap-2 text-xs/7 text-neutral-800" href="https://github.com/guiss26"><FiGithub className="text-neutral-500"/> github.com/guiss26</a>
-                    <a className="flex items-center gap-2 text-xs/7 text-neutral-800" href="https://www.linkedin.com/in/guissella-perez/"><FiLinkedin className="text-neutral-500"/> linkedin.com/in/guissella-perez</a>
+                    <a className="flex items-center gap-2 text-xs/7 text-neutral-800 dark:text-neutral-400" href="mailto:guissellapl@gmail.com"><MdOutlineMailOutline className="text-neutral-500"/> guissellapl@gmail.com</a>
+                    <a className="flex items-center gap-2 text-xs/7 text-neutral-800 dark:text-neutral-400" href="https://github.com/guiss26"><FiGithub className="text-neutral-500"/> github.com/guiss26</a>
+                    <a className="flex items-center gap-2 text-xs/7 text-neutral-800 dark:text-neutral-400" href="https://www.linkedin.com/in/guissella-perez/"><FiLinkedin className="text-neutral-500"/> linkedin.com/in/guissella-perez</a>
                 </div>
 
-                <div className="md:w-3/5 py-15 px-10">
-                    <form onSubmit={handleSubmit(onSubmit)} noValidate id="form" className="border p-6 bg-white text-xs tracking-widest shadow-[5px_5px_0px_0px_rgba(23,23,23,1)] transition-all duration-200 hover:-translate-y-0.5 hover:-translate-x-0.5 hover:shadow-[6px_6px_0px_0px_rgba(23,23,23,1)]">
+                <div className="md:w-3/5 py-15 px-10 dark:md:border-b dark:md:border-neutral-200">
+                    <form onSubmit={handleSubmit(onSubmit)} noValidate id="form" className="border dark:border-neutral-200 p-6 bg-white dark:bg-neutral-900 dark:text-neutral-400 text-xs tracking-widest shadow-[5px_5px_0px_0px_rgba(23,23,23,1)] transition-all duration-200 hover:-translate-y-0.5 hover:-translate-x-0.5 hover:shadow-[6px_6px_0px_0px_rgba(23,23,23,1)] dark:shadow-[5px_5px_0px_0px_rgba(255,255,255,1)] dark:hover:shadow-[6px_6px_0px_0px_rgba(255,255,255,1)]">
                         <div className="flex justify-between text-neutral-400 border-b pb-2">
                             <p>COMPOSE · NUEVO MENSAJE</p>
                             <p>~ / INBOX</p>
@@ -80,7 +80,7 @@ export default function Contact() {
                             <div className="flex flex-col sm:flex-row gap-4 mb-5">
                                 <div className="w-full sm:w-1/2 flex flex-col gap-2">
                                     <label htmlFor="name" className="text-neutral-500">NOMBRE *</label>
-                                    <input id="name" type="text" className="bg-neutral-300/40 border p-2 w-full" placeholder="Franco Lopez" {...register("name", { 
+                                    <input id="name" type="text" className="bg-neutral-300/40 dark:bg-neutral-950 border p-2 w-full" placeholder="Franco Lopez" {...register("name", { 
                                         required: "Este campo es requerido", 
                                         pattern: {
                                             value: /^[A-Za-zÀ-ÿ\s]+$/,
@@ -93,7 +93,7 @@ export default function Contact() {
                                 </div>
                                 <div className="w-full sm:w-1/2 flex flex-col gap-2">
                                     <label htmlFor="email" className="text-neutral-500">EMAIL *</label>
-                                    <input type="email" id="email" className="bg-neutral-300/40 border p-2 w-full" placeholder="example@empresa.com" {...register("email", { 
+                                    <input type="email" id="email" className="bg-neutral-300/40 dark:bg-neutral-950 border p-2 w-full" placeholder="example@empresa.com" {...register("email", { 
                                         required: "Este campo es requerido",
                                         pattern: {
                                             value: /^\S+@\S+\.\S+$/,
@@ -108,12 +108,12 @@ export default function Contact() {
 
                             <div className="flex flex-col gap-2 mb-5">
                                 <label htmlFor="subject" className="text-neutral-500">ASUNTO</label>
-                                <input id="subject" type="text" className="bg-neutral-300/40 border p-2" placeholder="Oportunidad junior en..." {...register("subject")} />
+                                <input id="subject" type="text" className="bg-neutral-300/40 dark:bg-neutral-950 border p-2" placeholder="Oportunidad junior en..." {...register("subject")} />
                             </div>
 
                             <div className="flex flex-col gap-2 mb-5">
                                 <label htmlFor="message" className="text-neutral-500">MENSAJE *</label>
-                                <textarea id="message" maxLength={400} rows={4} className="bg-neutral-300/40 border p-2" placeholder="..." {...register("message", { required: "Este campo es requerido" })}></textarea>
+                                <textarea id="message" maxLength={4000} rows={4} className="bg-neutral-300/40 dark:bg-neutral-950 border p-2" placeholder="..." {...register("message", { required: "Este campo es requerido" })}></textarea>
                                 {errors.message && (
                                     <span className="text-red-500 normal-case tracking-normal">{errors.message.message}</span>
                                 )}
@@ -121,8 +121,8 @@ export default function Contact() {
                         </fieldset>
 
                         <div className="flex justify-between items-center">
-                            <p className="text-neutral-400">{messageValue.length}/400</p>
-                            <button className="uppercase flex items-center gap-2 text-lime-400 border bg-neutral-950 py-2 px-4 hover:text-neutral-950 hover:bg-lime-400 hover:border border-neutral-950" disabled={loading}>
+                            <p className="text-neutral-400">{messageValue.length}/4000</p>
+                            <button className="uppercase flex items-center gap-2 text-lime-400 border bg-neutral-950 dark:bg-neutral-200 dark:hover:bg-lime-400 dark:text-black dark:hover:text-black py-2 px-4 hover:text-neutral-950 hover:bg-lime-400 hover:border border-neutral-950" disabled={loading}>
                                 {loading ? "Enviando..." : "Enviar mensaje"}    
                                  <TiLocationArrowOutline size={17}/></button>
                         </div>
